9ac32976962d27d0cf4723613f6ced374814f1c1
Korhan <dumn_korhan@hotmail.com>
2022-03-14 18:11:15 +0300

    Yıldız Oluşturma Programı yapıldı.

---
+++ src/Yildiz.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Yildiz {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Basamak sayısını giriniz : ");
+        int n = inp.nextInt();
+
+        for (int i = 1 ; i<=n ; i++) {
+            for (int k = 1 ; k<= (n - i); k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1 ; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+        for (int i = n ; i>0 ; i--) {
+            for (int k = 1; k < (n - i + 2); k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0 ; j < (i * 2 - 3) ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
