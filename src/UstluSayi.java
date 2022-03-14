4725b9c9e3b7eb7eefb2dff0035322835a1b73da
Korhan <dumn_korhan@hotmail.com>
2022-03-14 15:21:32 +0300

    Üstlü Sayı Program yapıldı.

---
+++ src/UstluSayi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UstluSayi {
+    public static void main(String[] args) {
+        int n;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Sınır sayısını giriniz : ");
+        n = inp.nextInt();
+
+        System.out.println("4'ün kuvvetleri : " );
+        for (int i =1 ; i<= n ; i *= 4) {
+            System.out.println(i);
+
+        }
+        System.out.println("5'in kuvvetleri : " );
+        for (int i =1 ; i<= n ; i *= 5) {
+            System.out.println(i);
+
+        }
+    }
+}
